6732989ec385339afbebc6da821337a6bfbdcc4d
Tirth Kanani <tirthkanani1485@gmail.com>
2026-04-20 12:02:48 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -4,8 +4,16 @@ import joblib
 import numpy as np
 
 # Load
-model = joblib.load("../model/cancer_model.pkl")
-scaler = joblib.load("../model/scaler.pkl")
+import os
+import joblib
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+model_path = os.path.join(BASE_DIR, "model", "cancer_model.pkl")
+scaler_path = os.path.join(BASE_DIR, "model", "scaler.pkl")
+
+model = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
 
 def predict(radius, texture, area):
     data = np.array([[radius, texture, area]])
